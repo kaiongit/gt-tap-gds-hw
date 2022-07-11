93be5606a7121ea64576be2070b3c95065c6e979
kaiongit <60166885+kaiongit@users.noreply.github.com>
2022-07-11 16:00:08 +0800

    Code cleanup - Add documentation - Add type hints - Strip constants - Return RESTful status codes

--- api/expander/main.py
+++ api/expander/main.py
@@ -1,47 +1,64 @@
-from flask import jsonify, make_response
+from flask import jsonify
 from google.cloud import firestore
 from google.cloud.firestore_v1.base_document import DocumentSnapshot
 from google.cloud.firestore_v1.collection import CollectionReference
 
-import functions_framework
+GCLOUD_PROJECT_NAME: str = "nomadic-thinker-355706"
 
-# add docs please
-# add types to variable
-# change value errors to just numbers
+import functions_framework  # Only for local development, do not include in deployment
 
-@functions_framework.http
+@functions_framework.http # Only for local development, do not include in deployment
 def expand_url_http(request):
 
-    content_type = request.headers["content-type"]
+    # Complain if payload is not JSON
+    try:
+        content_type: str = request.headers["content-type"]
+    except KeyError:
+        return jsonify({"msg": "Unknown payload type"}), 400
 
     if content_type != "application/json":
-        raise ValueError("Unknown content type: {}".format(content_type))
+        return jsonify({"msg": "Unknown payload type"}), 400
         
-    request_json = request.get_json(silent=True)
-
+    # Complain if payload does not contain "short" property
+    request_json: dict = request.get_json(silent=True)
     if not (request_json) or not ("short" in request_json):
-        raise ValueError("JSON is invalid, or missing a 'short' property")
+        return jsonify({"msg": "Payload is invalid, or does not contain property 'short'"}), 400
 
-    short = request_json["short"]
-
-    db = firestore.Client(project="nomadic-thinker-355706")
+    # Check DB if document exists
+    db: firestore.Client = firestore.Client(project=GCLOUD_PROJECT_NAME)
     col_ref: CollectionReference = db.collection("urls")
 
-    doc = get_doc_or_none(col_ref, short)
+    short: str = request_json["short"]
 
+    doc: DocumentSnapshot = get_doc_or_none(col_ref, short)
+
+    # If document does not exists, return
     if not doc:
-        return code_only_error(404)
+        return jsonify({"msg": "Cannot find document"}), 404
+
+    # If document exists, return 200
     else:
-        long = doc.to_dict()["url"]
-
-    return jsonify({
-        "short": short,
-        "long": long        
-    })
-
-def code_only_error(status_code: int):
-    return make_response("", status_code)
+        long: str = doc.to_dict()["url"]
+        return jsonify({
+            "short": short,
+            "long": long        
+        }), 200
 
 def get_doc_or_none(col_ref: CollectionReference, short: str) -> DocumentSnapshot:
+    """
+    Turns a hyperlink into a Firestore compliant document name by percent encoding then removing
+    forward slashes
+
+    Parameters
+    ----------
+    col_ref: CollectionReference
+        The reference to the collection to check in
+    short: str
+        The short path to check
+
+    Return
+    ------
+    The document or None
+    """
     docs = col_ref.where("short", "==", short).limit(1).get()
     return docs[0] if len(docs) > 0 else None
